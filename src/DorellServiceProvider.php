1170450b6837c163245beda5cdfa91f6e2e00ce3
Alihan Öztürk <alihanozturk364@gmail.com>
2023-04-14 20:06:42 +0300

    provider contetn

---
+++ src/DorellServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+ 
+namespace Alihan0\LaravelDorell;
+ 
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+ 
+class DorellServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        $this->info("provider okundu!");
+    }
+}
